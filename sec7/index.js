f0cd383dc42ba1f7f09f2f08248668ab7eb34c18
Adarsh <protoyoru07@gmail.com>
2024-12-19 09:10:47 +0530

    update sec7 !!

--- sec7/index.js
+++ sec7/index.js
@@ -23,20 +23,12 @@ const friends = [
 server.on('request',(req,res)=>{
   const items=req.url.split('/');
   //" ","friends","id",
-  if(items[1]==='friends'){
-    res.statusCode = 200;
-    res.setHeader('Content-Type','application/json');
-    if(items.length ===3){
-      const index = Number(items[2]);
-      if(index>friends.length){
-        res.statusCode = 404;
-        res.end();
-      }
-      res.end(JSON.stringify(friends[index]));
-    }
-    else{
-      res.end(JSON.stringify(friends));
-    }
+  if(items[1]==='friends' && req.method === 'POST'){
+    req.on('data',(data)=>{
+      const friend = data.toString();
+      console.log('Request: ',friend);
+      friends.push(JSON.parse(friend));
+    });
   }
   else{
     res.statusCode = 404;
