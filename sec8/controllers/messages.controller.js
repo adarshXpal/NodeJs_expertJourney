38c9f8772dc86c19a4b86e542138fd916aaa9c6e
Adarsh <protoyoru07@gmail.com>
2024-12-26 14:33:55 +0530

    Section 8 complete

--- sec8/controllers/messages.controller.js
+++ sec8/controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path =require('path');
+
 function getMessages(req,res){
-  res.send('<ul><li>HELLO ADARSH !!</li></ul>');
+  // res.send('<ul><li>HELLO ADARSH !!</li></ul>');
+  res.sendFile(path.join(__dirname,'..','public','images','adarsh_photo.png'));
 };
 function postMessages(req,res){
   console.log(`Updating Messages !!`);
--- sec8/server.js
+++ sec8/server.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = 3001;
 const friendsRouter = require('./Routers/friends.router');
 const messagesRouter = require('./Routers/messages.router');
+const path = require('path');
 
+app.set('view engine','hbs');
+app.set('views',path.join(__dirname,'view'));
 
 app.use((req,res,next)=>{
   const start = Date.now();
@@ -11,9 +14,15 @@ app.use((req,res,next)=>{
   const delta = Date.now()-start;
   console.log(`${req.method}${req.baseUrl}\n ${req.url} ${delta} milliseconds`);
 });
+app.use('/site',express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use('/friends',friendsRouter);
 app.use('/messages',messagesRouter);
+app.get('/',(req,res)=>{
+  res.render('index',{
+    title: 'My Friends Are Very Clever !!',
+    caption:'My name is Adarsh!!', });
+});
 app.listen(PORT,()=>{
   console.log(`Listning at PORT: ${PORT}`);
 });
