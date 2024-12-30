75ab9adf2a9c323647bba0ac4f64b64c5f9725eb
Adarsh <protoyoru07@gmail.com>
2024-12-30 14:32:32 +0530

    Final Name change !!

--- sec9/server/src/routes/planets/planets.router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const planetsRouter = express.Router();
-const {getAllPlanets,
-} = require('./planets.controller');
-
-planetsRouter.get('/planets',getAllPlanets);
-
-module.exports = planetsRouter;
